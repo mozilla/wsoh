9268004fdf6cad7221276dcecdbb8694d0d77db7
Owen Coutts <owen@owencoutts.com>
2011-07-21 00:51:42 -0700

    Starting to piece together nowjs and express

---
+++ node/server.js
@@ -0,0 +1,74 @@
+﻿var
+  path = require('path'),
+  express = require('express')
+
+  PORT = 8003,
+  WEBROOT = path.join(path.dirname(__filename), 'webroot');
+
+server = express.createServer()
+server.use('/static',express.static(WEBROOT));
+
+server.get('/', function(req,res){
+  res.send('Hello World');
+});
+server.listen(PORT)
+console.log('Server running on port ' + PORT)
+
+
+var nowjs = require("now");
+var everyone = nowjs.initialize(server);
+
+static_serve = function(WEBROOT, req, res){
+  var ip = req.connection.remoteAddress;
+  paperboy
+    .deliver(WEBROOT, req, res)
+    .addHeader('Expires', 300)
+    .addHeader('X-PaperRoute', 'Node')
+    .before(function() {
+      console.log('Received Request');
+    })
+    .after(function(statCode) {
+      log(statCode, req.url, ip);
+    })
+    .error(function(statCode, msg) {
+      res.writeHead(statCode, {'Content-Type': 'text/plain'});
+      res.end("Error " + statCode);
+      log(statCode, req.url, ip, msg);
+    })
+    .otherwise(function(err) {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end("Error 404: File not found");
+      log(404, req.url, ip, err);
+    });
+}
+
+function log(statCode, url, ip, err) {
+  var logStr = statCode + ' - ' + url + ' - ' + ip;
+  if (err)
+    logStr += ' - ' + err;
+  console.log(logStr);
+}
+
+
+nowjs.on('connect', function(){
+  this.now.room = "room 1";
+  nowjs.getGroup(this.now.room).addUser(this.user.clientId);
+  console.log("Joined: " + this.now.name);
+});
+
+
+nowjs.on('disconnect', function(){
+  console.log("Left: " + this.now.name);
+});
+
+everyone.now.changeRoom = function(newRoom){
+  nowjs.getGroup(this.now.room).removeUser(this.user.clientId);
+  nowjs.getGroup(newRoom).addUser(this.user.clientId);
+  this.now.room = newRoom;
+  this.now.receiveMessage("SERVER", "You're now in " + this.now.room);
+}
+
+everyone.now.distributeMessage = function(message){
+  nowjs.getGroup(this.now.room).now.receiveMessage(this.now.name, message);
+};
+
