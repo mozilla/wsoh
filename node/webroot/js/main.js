fe0833a118a51a8509f700a5fbdcc05527e1a94a
Adrian Gaudebert <adrian@gaudebert.fr>
2011-07-23 04:56:12 -0700

    Removing unneeded stuff.

--- node/webroot/js/main.js
+++ node/webroot/js/main.js
@@ -1,20 +1,6 @@
-function log(msg) {
-    $("#logs").append("<li>"+msg+"</li>");
-}
-
 $(document).ready(function(){
-  now.roomID=window.location.pathname.split('/')[2]
-  now.setup = function(playlist){
-      $('div').text(playlist);
-  }
-
-  now.consoleOut = function(message){
-    $('div').text(message)
-    console.debug(message)
-  }
-
-  $("#send-button").click(function(){
-    now.updatePlaylist($("#text-input").val());
-    $("#text-input").val("");
-  });
+    now.roomID = window.location.pathname.split('/')[2]
+    now.setup = function(playlist){
+        audioPlaylist.playlistChange(playlist);
+    }
 });
