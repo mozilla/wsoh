5892a5aebace323deeb660b710ba521f2f823420
Adrian Gaudebert <adrian@gaudebert.fr>
2011-07-23 04:56:27 -0700

    Removing logs.

--- node/webroot/js/current-player.js
+++ node/webroot/js/current-player.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     now.someoneWantsToBeThePlayer = function(clientId) {
-        log("someoneWantsToBeThePlayer: " + clientId);
         var accept = confirm("Someone wants to be the new Player, do you agree on not being the Player anymore?");
         if (accept) {
             now.changeThePlayerTo(clientId);
@@ -8,25 +7,21 @@ $(document).ready(function() {
     }
 
     now.youAreThePlayerNow = function() {
-        log("youAreThePlayerNow");
         // Do stuff that make you the player now
-        alert("Yeah I'm the player now!");
+        audioPlaylist.do();
     }
 
     now.youAreTheCurrentPlayer = function() {
-        log("youAreTheCurrentPlayer");
         // Do stuff that say you're the current player
         alert("I am the current player.");
     }
 
     now.youAreNotTheCurrentPlayer = function() {
-        log("youAreNotTheCurrentPlayer");
         // Do stuff that say you're not the current player
         alert("I am NOT the current player.");
     }
 
     $("#become-player").click(function () {
-        log("clicked iWantToBeThePlayer");
         now.iWantToBeThePlayer();
     });
 });
