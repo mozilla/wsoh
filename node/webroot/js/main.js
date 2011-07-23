909e2c333cda5b1fc25772797e215240af6b1c94
Adrian Gaudebert <adrian@gaudebert.fr>
2011-07-23 05:10:05 -0700

    Pushing functions out of document ready.

--- node/webroot/js/main.js
+++ node/webroot/js/main.js
@@ -1,6 +1,4 @@
-$(document).ready(function(){
-    now.roomID = window.location.pathname.split('/')[2]
-    now.setup = function(playlist){
-        audioPlaylist.playlistChange(playlist);
-    }
-});
+now.roomID = window.location.pathname.split('/')[2];
+now.setup = function(playlist) {
+    audioPlaylist.playlistChange(playlist);
+}
