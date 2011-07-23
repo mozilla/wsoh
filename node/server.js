6caef6aa41b2a739c5c2996aafbed8a0a85a7c60
Adrian Gaudebert <adrian@gaudebert.fr>
2011-07-22 21:28:20 -0700

    Using nowjs groups for rooms.

--- node/server.js
+++ node/server.js
@@ -4,12 +4,13 @@
   nowjs = require("now"),
   express = require("express"),
   redislib = require("redis"),
+  util = require("util"),
 
   PORT = 8003,
   WEBROOT = path.join(path.dirname(__filename), '/webroot'),
   redis = redislib.createClient();
-      
-//Error handler for REDIS    
+
+//Error handler for REDIS
 redis.on("error", function (err) {
     console.log("Redis connection error to " + redis.host + ":" + redis.port + " - " + err);
 });
@@ -28,8 +29,8 @@ server.use('/static',express.static(WEBROOT));
 //Define route for a playlist
 server.get('/playlist/*',function(req, response){
   fs.readFile(WEBROOT+'/index.html', function(err, data){
-    response.writeHead(200, {'Content-Type':'text/html'}); 
-    response.write(data);  
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.write(data);
     response.end();
   });
 });
@@ -46,19 +47,23 @@ function set_playlist(ID,playlist){
   redis.SET('playlist:'+ID,playlist);
 }
 
-//Server side listener for global distrobution
+// Add a user to a group
+function joinGroup(groupId, client) {
+    group = nowjs.getGroup(groupId);
+    group.addUser(client.user.clientId);
+    util.log("Client " + client.user.clientId + " was added to group " + groupId);
+}
+
 everyone.now.distributeMessage = function(message){
-  set_playlist(this.now.roomID,message)
-  everyone.now.printToConsole(message,this.now.roomID)
-};
+    set_playlist(this.now.roomID, message);
+    nowjs.getGroup(this.now.roomID)
+        .now.consoleOut(message);
+}
+    //~ everyone.now.printToConsole(message,this.now.roomID);
 
-//Filters and prints to a playlist
-everyone.now.printToConsole = function(message, targetRoomId){
-  if(targetRoomId == this.now.roomID){
-    this.now.consoleOut(message);
-  }
-};
+//Server side listener for global distrobution
 
-everyone.on('connect', function(){
-  redis_get('playlist:'+this.now.roomID,this.now.setup)
+everyone.on('connect', function() {
+    joinGroup(this.now.roomID, this);
+    redis_get('playlist:'+this.now.roomID,this.now.setup)
 });
