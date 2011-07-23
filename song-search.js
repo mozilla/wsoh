57c38897ef9add1aa9c85421e05012734c1bb9bf
max kanter <max@maxkanter.(none)>
2011-07-22 18:02:01 -0700

    Put previously made song-search.js into an acutally function page.
    It does  google instant-esque searching which populates list when you pause at all in your typing. must be running ruby server for it to work.

--- song-search.js
+++ song-search.js
@@ -1,17 +1,35 @@
-var search = 'test song name' //probably want to use the following to get text from search field $('#elementID').val(); 
+<html>
+<head>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script> 
+<script>
+$(document).ready ( function {
+	$('#song-search-box').keyup(function () {
+		var search = this.val;
+		$.get('http://localhost:4567/search?q='+search, function (response) {
+			response = $.parseJSON(response);	
+			for (var i=0,max=response.length;i<max;i++) { //iterates through each item of response 
+				song = response[i];
+				track = song.track;
+				artistID = song.artistID;
+				songName = song.songName;
+				albumID = song.albumID;
+				albumName = song.albumName;
+				artworkUrl = song.artworkUrl;
+				artistName = song.artistName;
+				songID = song.songID
+				console.log(artistName);
+			}
+		})
+	})
 
-$.get('http://localhost:4567/search?q='+search, function (response) {
-	response = $.parseJSON(response);	
-	for (var i=0,max=response.length;i<max;i++) { //iterates through each item of response 
-		song = response[i];
-		track = song.track;
-		artistID = song.artistID;
-		songName = song.songName;
-		albumID = song.albumID;
-		albumName = song.albumName;
-		artworkUrl = song.artworkUrl;
-		artistName = song.artistName;
-		songID = song.songID
-		console.log(artistName);
-	}
-})
+	
+}
+</script>
+</head>
+<body>
+<input id="song-search-box" placeholder="Add a song!"></input>
+<div id="search-results">
+
+</div>
+</body>
+</html>
